140b1e5ab57dfd053aadd078063686d4b8c793b2
Rafal Bednarczuk <rafbednarczuk@gmail.com>
2024-10-11 19:06:00 +0200

    Switch to mainnet

--- src/hooks/useTonApiClient.ts
+++ src/hooks/useTonApiClient.ts
@@ -2,14 +2,14 @@ import {useAsyncInitialize} from './useAsyncInitialize';
 import {Api, TonApiClient} from '@ton-api/client';
 
 export function useTonApiClient() {
-  return useAsyncInitialize(
-    async () => {
-        const http = new TonApiClient({
-            baseUrl: 'https://testnet.tonapi.io',
-            //TODO: switch to domain & IP restricted api key
-            apiKey: 'AE4VKCTFFJ2Q5YYAAAAORBMHSCNQWDX4J5TH7MKLOQZ6YNG4SUL5GNQSOFKSXNHAD5D2IMQ'
-        });
-        return new Api(http);
-    }
-  );
+    return useAsyncInitialize(
+        async () => {
+            const http = new TonApiClient({
+                baseUrl: 'https://tonapi.io',
+                //TODO: switch to domain & IP restricted api key
+                apiKey: 'AE4VKCTFFJ2Q5YYAAAAORBMHSCNQWDX4J5TH7MKLOQZ6YNG4SUL5GNQSOFKSXNHAD5D2IMQ'
+            });
+            return new Api(http);
+        }
+    );
 }
